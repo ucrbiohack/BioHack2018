56602dbda30e486c039012aa2bbabea57d46841b
John Pham <johnphammail@gmail.com>
2018-02-23 18:05:09 -0800

    adding bmes logo, adding static navbar, adding smooth scrolling

--- js/main.js
+++ js/main.js
@@ -126,6 +126,12 @@ function scaleBannerVideoSize(element){
 	};
 	contentWayPoint();
 
-
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 500);
+    });
 
 })(jQuery);
